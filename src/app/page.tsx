971dcab7e66e49c6e4676df7d1ac9792dea94812
palak4207 <palak4207@gmail.com>
2025-02-11 15:58:26 +0530

    fix: removed cache

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import Header from "@/components/Header/Header";
 import Projects from "@/components/Projects/Projects";
 
 export default async function Home() {
-  const allProjects = await fetch(`${process.env.BASE_URL}/projects`);
+  const allProjects = await fetch(`${process.env.BASE_URL}/projects`, {
+    cache: "no-store",
+  });
   // const data = await allProjects.json();
   // console.log("data", data);
   // return (
