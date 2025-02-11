9d0ac67086a9bad491fc683cc4b2f6411e1cd1d2
palak4207 <palak4207@gmail.com>
2025-02-11 15:32:47 +0530

    fix: try catch conditions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
-import Header from "@/components/Header/Header";
 import Projects from "@/components/Projects/Projects";
 
 export default async function Home() {
   const allProjects = await fetch(`${process.env.BASE_URL}/projects`);
-  const data = await allProjects.json();
-  return (
-    <>
-      <Header />
-      {data?.length > 0 && <Projects data={data} />}
-    </>
-  );
+  // const data = await allProjects.json();
+  // console.log("data", data);
+  // return (
+  //   <>
+  //     <Header />
+  //     {data?.length > 0 && <Projects data={data} />}
+  //   </>
+  // );
+
+  try {
+    const data = await allProjects.json();
+    return <div>{data?.length > 0 && <Projects data={data} />}</div>;
+  } catch (err) {
+    console.error("Error parsing JSON:", err);
+    return <div>Error parsing data</div>; // Show an error message
+  }
 }
