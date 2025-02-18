aa8be02e3c7b15c938f3eeef9a66a3a691172444
palak4207 <palak4207@gmail.com>
2025-02-18 18:09:55 +0530

    fix: css fixes of contact page

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 const Contact = () => {
   return (
-    <>
-      <div className="relative w-full lg:h-screen bg-black text-white overflow-hidden">
+    <body className="bg-black">
+      <div className="relative w-full bg-black text-white overflow-hidden">
         <Header />
         <div className="mt-24 inset-0 flex flex-col lg:flex-row justify-between items-center px-6 md:px-16">
           <div className="flex flex-col space-y-8 w-full lg:w-1/3">
@@ -29,7 +29,7 @@ const Contact = () => {
             </div>
           </div>
 
-          <div className="relative w-full h-[300px] md:w-[500px] md:h-[500px] rounded-full overflow-hidden border border-gray-700 mt-6 lg:mt-0">
+          <div className="relative w-full h-[300px] md:w-[500px] md:h-[500px] m-8 rounded-full overflow-hidden border border-gray-700 mt-6 lg:mt-0">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6471.386157975174!2d75.06516004388904!3d24.07670456916714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39642d4e43b0ef4f%3A0x749f67563171622c!2sLandscape%20Architects!5e0!3m2!1sen!2sin!4v1734865623627!5m2!1sen!2sin&maptype=roadmap&disableDefaultUI=true"
               width="100%"
@@ -84,7 +84,7 @@ const Contact = () => {
           <p>© 2019. All rights reserved.</p>
         </footer> */}
       </div>
-    </>
+    </body>
   );
 };
 
