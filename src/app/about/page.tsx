3072cfea0c9b8ecc902c76118b19d99abe189c40
palak4207 <palak4207@gmail.com>
2025-02-19 16:58:20 +0530

    fix: caching issue resolved

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,9 @@ import { ITeamInfo } from "@/components/common/interface";
 import React from "react";
 
 const page = async () => {
-  const data = await fetch(`${process.env.BASE_URL}/team`);
+  const data = await fetch(`${process.env.BASE_URL}/team`, {
+    cache: "no-store",
+  });
   const teamInfo: ITeamInfo[] = await data.json();
   return <div>{teamInfo?.length > 0 && <About teamInfo={teamInfo} />}</div>;
 };
