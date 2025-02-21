8f1097b8aa3b191cbc526c8e514beb74a6560090
palak4207 <palak4207@gmail.com>
2025-02-21 16:50:35 +0530

    fix: resolved cors error

--- src/app/api/admin/team/route.ts
+++ src/app/api/admin/team/route.ts
@@ -16,11 +16,6 @@ export async function POST(req: NextRequest) {
       }),
       {
         status: 201,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        },
       }
     );
   } catch (error) {
