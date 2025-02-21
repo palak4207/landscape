79e1813e4f275ae2bbe90fd0f229b156b7964e92
palak4207 <palak4207@gmail.com>
2025-02-21 12:53:09 +0530

    fix: resolved cors error

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,27 @@ const nextConfig: NextConfig = {
       "firebasestorage.googleapis.com",
     ], // Add hostnames here
   },
+
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Change * to your actual origin in production
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   /* config options here */
 };
 
